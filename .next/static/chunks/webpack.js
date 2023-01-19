9b3f5c35da5cc00f702784ca5f74a9fb19b218ab
BDKoussama <bdk.oussama@gmail.com>
2023-01-19 22:40:01 +0100

    updated logo svg

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2b55e41b65050c04"; }
+/******/ 		__webpack_require__.h = function() { return "6936075752244ccf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
