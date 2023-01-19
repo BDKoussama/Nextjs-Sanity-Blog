83bcc1eeac56f313e9074a25b70b7da447628bd1
BDKoussama <bdk.oussama@gmail.com>
2023-01-19 17:55:01 +0100

    updated sanity studio

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fae4de28543e5fbc"; }
+/******/ 		__webpack_require__.h = function() { return "94cf2029b40f1db9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ giftideas/schemas/index.js
@@ -0,0 +1,19 @@
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import author from './author'
+import product from './product'
+import AffiliateLink from './buyLink';
+import Policy from './privacy';
+import Terms from './terms';
+
+export const schemaTypes = [
+    blockContent,
+    category,
+    post,
+    author,
+    product,
+    AffiliateLink,
+    Policy,
+    Terms
+]
